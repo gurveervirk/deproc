fe1309d6af60ae649e0b707c9ea8740534f25b5d
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-05-04 23:11:55 +0530

    chore: Update __init__.py of parser interface for previous changes

--- deproc_core/interfaces/parser/__init__.py
+++ deproc_core/interfaces/parser/__init__.py
@@ -1,37 +1,5 @@
 from .base import SourceParser
-from .models import (
-    DecoratorInfo,
-    ClassVariable,
-    SourceFile,
-    ImportStmt,
-    FunctionLike,
-    Parameter,
-    TypeDeclaration
-)
-from .utils import (
-    iter_children,
-    first_child,
-    walk_preorder
-)
 
 __all__ = [
-    "SourceParser",
-    "DecoratorInfo",
-    "ClassVariable",
-    "SourceFile",
-    "ImportStmt",
-    "FunctionLike",
-    "Parameter",
-    "TypeDeclaration",
-    "get_parser_core",
-    "register_plugin",
-    "get_plugin_for_language",
-    "get_plugin_for_extension",
-    "registered_parser_plugins",
-    "get_registered_file_extensions",
-    "ignore_file_extensions",
-    "supported_file_extensions",
-    "iter_children",
-    "first_child",
-    "walk_preorder",
+    "SourceParser"
 ]
