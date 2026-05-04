430731a264dcab73307939213065661f4c2331d2
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-05-04 22:39:49 +0530

    chore: Add processor to __init__ exports

--- deproc_core/__init__.py
+++ deproc_core/__init__.py
@@ -3,10 +3,13 @@
 from .config import Config
 from .discovery import find_source_files
 from .runtime import RuntimeRegistry, get_runtime_registry
+from .processor import ProcessPathResult, process_path
 
 __all__ = [
     "get_runtime_registry",
     "RuntimeRegistry",
     "find_source_files",
     "Config",
+    "process_path",
+    "ProcessPathResult"
 ]
