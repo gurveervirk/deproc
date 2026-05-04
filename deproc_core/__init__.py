c53f7e27a6ac496f46eba385576abd2d303c864a
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-05-04 13:15:45 +0530

    chore: Update __init__.py in deproc_core

--- deproc_core/__init__.py
+++ deproc_core/__init__.py
@@ -1,14 +1,12 @@
 """deproc_core: language-agnostic core runtime."""
 
-from .api import get_registered_file_extensions, ignore_file_extensions, process_path
-from .interfaces.parser import ParserPlugin
+from .config import Config
+from .discovery import find_source_files
 from .runtime import RuntimeRegistry, get_runtime_registry
 
 __all__ = [
     "get_runtime_registry",
     "RuntimeRegistry",
-    "get_registered_file_extensions",
-    "ignore_file_extensions",
-    "ParserPlugin",
-    "process_path",
+    "find_source_files",
+    "Config",
 ]
