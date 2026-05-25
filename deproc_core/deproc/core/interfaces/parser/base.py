bb388cbbb46fb0022af021beae801db721f61563
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-05-25 21:24:05 +0530

    feat: Modify parse_file to add context arg to it

--- deproc_core/deproc/core/interfaces/parser/base.py
+++ deproc_core/deproc/core/interfaces/parser/base.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 from typing import Protocol, runtime_checkable
 from .models import SourceFile
+from ...context import Context
 
 @runtime_checkable
 class SourceParser(Protocol):
-    def parse_file(self, file_path: str) -> SourceFile:
+    def parse_file(self, file_path: str, context: Context) -> SourceFile:
         ...
