4704449b0911a8af3622f6bcef8e5ba584d90ab5
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-05-26 20:38:16 +0530

    chore: Setup tree sitter utils for deproc-python

---
+++ deproc_python/deproc/plugins/python/parser/utils/tree_sitter_python.py
@@ -0,0 +1,29 @@
+from tree_sitter import (
+    Language,
+    Node,
+    Parser
+)
+from ..models import SourceRange
+import tree_sitter_python
+
+def get_python_language() -> Language:
+    return Language(tree_sitter_python.language())
+
+def get_python_parser() -> Parser:
+    PY_LANGUAGE = get_python_language()
+    parser = Parser(PY_LANGUAGE)
+    return parser
+    
+def node_text(node: Node, source: bytes) -> str:
+    if not node:
+        return ""
+    return source[node.start_byte : node.end_byte].decode("utf-8")
+
+
+def create_source_range(node: Node) -> SourceRange:
+    return SourceRange(
+        lineno=node.start_point.row + 1,
+        end_lineno=node.end_point.row + 1,
+        col_offset=node.start_point.column,
+        end_col_offset=node.end_point.column
+    )
