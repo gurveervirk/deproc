8e6ab4387ff55c85c51be1b782660a852cff4585
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-06-18 21:13:12 +0530

    fix: Fix incorrect PythonResolver setup and imports

--- deproc_python/deproc/plugins/python/resolver/main.py
+++ deproc_python/deproc/plugins/python/resolver/main.py
@@ -1,9 +1,8 @@
-from deproc_core.deproc.core.interfaces.resolver import Resolver
-from deproc_core.deproc.core.context import Context
+from deproc.core.interfaces.resolver import Resolver
+from deproc.core.context import Context
 from .models import PythonResolverInput
 from .utils import resolve_symbol
-from ..symbol_table_builder.models import PythonSymbolTable
 
-class PythonResolver(Resolver[PythonSymbolTable[PythonResolverInput], list[str]]):
+class PythonResolver(Resolver[PythonResolverInput, list[str]]):
     def resolve(self, data_in: PythonResolverInput, context: Context) -> list[str]:
         return resolve_symbol(data_in, context)
