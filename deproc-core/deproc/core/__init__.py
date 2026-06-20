688ecdb005afd9a70ed64449d879b4c2a7c84ad6
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-06-20 23:52:56 +0530

    chore: Remove processor from __init__.py in deproc core

--- deproc-core/deproc/core/__init__.py
+++ deproc-core/deproc/core/__init__.py
@@ -1,18 +1,11 @@
-"""deproc_core: language-agnostic core runtime."""
-
-from .config import Config
 from .discovery import find_source_files
 from .runtime import (
     get_runtime_registry,
     LanguagePlugin
 )
-from .processor import ProcessPathResult, process_path
 
 __all__ = [
     "get_runtime_registry",
     "LanguagePlugin",
     "find_source_files",
-    "Config",
-    "process_path",
-    "ProcessPathResult"
 ]
