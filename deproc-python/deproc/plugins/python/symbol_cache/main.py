e7a6363b6c9043f3b304d7ed9e7009e24e976479
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-07-06 23:56:24 +0530

    feat: Add rev map cache_key_to_modules in python symbol cache and helper functions

--- deproc-python/deproc/plugins/python/symbol_cache/main.py
+++ deproc-python/deproc/plugins/python/symbol_cache/main.py
@@ -13,6 +13,7 @@ class PythonSymbolCache(SymbolCache[cache_key, cache_value]):
         self.language = "python"
         self.cache: dict[cache_key, cache_value] = {}
         self.module_to_cache_keys: dict[module_fqn, set[cache_key]] = {}
+        self.cache_key_to_modules: dict[cache_key, set[module_fqn]] = {}
 
     def get(self, module_fqn: module_fqn, symbol_name: symbol_name) -> cache_value | None:
         key: cache_key = (module_fqn, symbol_name)
@@ -21,6 +22,32 @@ class PythonSymbolCache(SymbolCache[cache_key, cache_value]):
     def set(self, module_fqn: module_fqn, symbol_name: symbol_name, resolved_ids: ResolvedIDs, unresolved_ids: UnresolvedIDs) -> None:
         key: cache_key = (module_fqn, symbol_name)
         self.cache[key] = (resolved_ids, unresolved_ids)
+
+    def add_cache_keys_for_module(self, module_fqn: module_fqn, keys: set[cache_key]) -> None:
+        if module_fqn not in self.module_to_cache_keys:
+            self.module_to_cache_keys[module_fqn] = set()
+        self.module_to_cache_keys[module_fqn].update(keys)
+
+        for key in keys:
+            if key not in self.cache_key_to_modules:
+                self.cache_key_to_modules[key] = set()
+            self.cache_key_to_modules[key].add(module_fqn)
+    
+    def get_cache_keys_for_module(self, module_fqn: module_fqn) -> set[cache_key]:
+        return self.module_to_cache_keys.get(module_fqn, set())
+    
+    def add_modules_for_cache_key(self, key: cache_key, modules: set[module_fqn]) -> None:
+        if key not in self.cache_key_to_modules:
+            self.cache_key_to_modules[key] = set()
+        self.cache_key_to_modules[key].update(modules)
+
+        for module in modules:
+            if module not in self.module_to_cache_keys:
+                self.module_to_cache_keys[module] = set()
+            self.module_to_cache_keys[module].add(key)
+    
+    def get_modules_for_cache_key(self, key: cache_key) -> set[module_fqn]:
+        return self.cache_key_to_modules.get(key, set())
 
     def clear(self) -> None:
         self.cache.clear()
