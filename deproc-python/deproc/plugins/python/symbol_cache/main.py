3c9bac9e9915cec68894e0d58c9b95f760ee3eb4
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-07-06 22:45:54 +0530

    feat: Add a module_to_cache_keys map in python symbol cache

--- deproc-python/deproc/plugins/python/symbol_cache/main.py
+++ deproc-python/deproc/plugins/python/symbol_cache/main.py
@@ -12,6 +12,7 @@ class PythonSymbolCache(SymbolCache[cache_key, cache_value]):
     def __init__(self):
         self.language = "python"
         self.cache: dict[cache_key, cache_value] = {}
+        self.module_to_cache_keys: dict[module_fqn, set[cache_key]] = {}
 
     def get(self, module_fqn: module_fqn, symbol_name: symbol_name) -> cache_value | None:
         key: cache_key = (module_fqn, symbol_name)
@@ -23,3 +24,11 @@ class PythonSymbolCache(SymbolCache[cache_key, cache_value]):
 
     def clear(self) -> None:
         self.cache.clear()
+        self.module_to_cache_keys.clear()
+
+    def clear_module(self, module_fqn: module_fqn) -> None:
+        if module_fqn in self.module_to_cache_keys:
+            for key in self.module_to_cache_keys[module_fqn]:
+                if key in self.cache:
+                    del self.cache[key]
+            del self.module_to_cache_keys[module_fqn]
