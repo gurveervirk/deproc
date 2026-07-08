43e290a1ab95b2979d6f41d71161b57374dc0135
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-07-08 20:24:38 +0530

    chore: Update deproc-python to expose implementation directly, instead of demised plugin

--- deproc-python/deproc/plugins/python/__init__.py
+++ deproc-python/deproc/plugins/python/__init__.py
@@ -1,3 +1,13 @@
-from .core import PythonPlugin
+from .linker import PythonLinker
+from .parser import PythonSourceParser
+from .resolver import PythonResolver
+from .symbol_cache import PythonSymbolCache
+from .symbol_table_builder import PythonSymbolTableBuilder
 
-__all__ = ["PythonPlugin"]
+__all__ = [
+    "PythonLinker",
+    "PythonResolver",
+    "PythonSourceParser",
+    "PythonSymbolCache",
+    "PythonSymbolTableBuilder",
+]
