16f6fab46de445047c686d5658966e9d32a7e7fe
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-08-02 15:42:09 +0530

    chore: Move some export utils to deproc and refactor utils in deproc python

---
+++ deproc-python/deproc/plugins/python/utils/exports.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from collections import defaultdict
+
+if TYPE_CHECKING:
+    from deproc.core.runtime.registries.entity import EntityRegistry
+    from ..parser.models import PythonModule
+
+def build_module_exports(registry: EntityRegistry) -> dict[str, set[str]]:
+    exports: dict[str, set[str]] = defaultdict(set)
+    for entity in registry.values():
+        if isinstance(entity, PythonModule) and hasattr(entity, "all_exports") and entity.all_exports:
+            for name in entity.all_exports:
+                exports[entity.fqn].add(name)
+    return dict(exports)
