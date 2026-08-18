9a3c968fddca5a3635c088202f69de3f6b5d8442
Gurveer Singh Virk <gurveervirk.15517@gmail.com>
2026-08-18 20:33:34 +0530

    chore: Widen core parser and linker to use Node instead of SourceFile for params/args

--- deproc-core/deproc/core/interfaces/parser/base.py
+++ deproc-core/deproc/core/interfaces/parser/base.py
@@ -4,9 +4,9 @@ from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
 if TYPE_CHECKING:
     from ...context import Context
-    from .models import SourceFile
+    from .models import Node
 
 
 @runtime_checkable
 class SourceParser(Protocol):
-    def parse_file(self, file_path: str, context: Context) -> SourceFile: ...
+    def parse_file(self, file_path: str, context: Context) -> Node: ...
